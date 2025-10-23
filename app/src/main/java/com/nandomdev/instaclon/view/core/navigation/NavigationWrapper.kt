46fc7975123d73577d5e74e185a9b23b2b113d7b
Nando Miranda <ingfermiranda@gmail.com>
2025-10-23 14:54:10 -0600

    refactor: se ajustan import´s de archivos locales.

--- app/src/main/java/com/nandomdev/instaclon/view/core/navigation/NavigationWrapper.kt
+++ app/src/main/java/com/nandomdev/instaclon/view/core/navigation/NavigationWrapper.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.nandomdev.instaclon.view.auth.LoginScreen
+import com.nandomdev.instaclon.view.auth.login.LoginScreen
 
 @Composable
 fun NavigationWrapper() {
