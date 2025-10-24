bff2fd23a4292142f4a081b7791afd7e7313efec
Nando Miranda <ingfermiranda@gmail.com>
2025-10-23 18:15:22 -0600

    feat: nuevos colores para app instaClon.

--- app/src/main/java/com/nandomdev/instaclon/ui/theme/Color.kt
+++ app/src/main/java/com/nandomdev/instaclon/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//InstaClon Colors
+val InstaBlue = Color(0xFF0062DD)
+val Gray20 = Color(0xFF152125)
+val Gray85 = Color(0xFF8EA0B0)
+val white95 = Color(0xFFFDFDFD)
+val Gray75 = Color(0xFF5B6A79)
